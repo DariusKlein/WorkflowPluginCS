57578edeff107af8810bcdae1404263596f5e3ac
Darius <darius.klein@student.hu.nl>
2023-06-13 12:59:27 +0200

    commit

--- WorkflowPlugin/Properties/AssemblyInfo.cs
+++ WorkflowPlugin/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WorkflowPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Personal workflow plugin")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WorkflowPlugin")]
